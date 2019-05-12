361e01f318b8b27bd369552b1ac9ef77f9269460
WarrenF <warren.fitzpatrick@holidayextras.com>
2019-05-12 22:05:04 +0100

    madProps!

--- index.js
+++ index.js
@@ -1,4 +1,56 @@
-module.exports = {
-  internalUrl: require('./validators/internalUrl'),
-  htmlId: require('./validators/htmlId')
+const madProps = (customValidations = {}, props, propName, componentName = 'Anon') => {
+  const value = props[propName]
+
+  const {
+    func,
+    regex,
+    isRequired,
+    minLength,
+    maxLength
+  } = customValidations
+
+  if (!isRequired && !value) return null
+
+  if (isRequired && !value) return new TypeError(`${propName} is required but has no value in ${componentName}`)
+
+  if (
+    minLength
+    && typeof minLength === 'number'
+    && (
+      typeof value === 'string'
+      || value instanceof Array
+    )
+    && value.length < minLength
+  ) {
+    return new TypeError(`${propName} is less than min length of ${minLength} characters in ${componentName}`);
+  }
+
+  if (
+    maxLength
+    && typeof maxLength === 'number'
+    && (
+      typeof value === 'string'
+      || value instanceof Array
+    )
+    && value.length > maxLength
+  ) {
+    return new TypeError(`${propName} is more than the max length of ${maxLength} characters in ${componentName}`);
+  }
+
+  if (
+    regex
+    && regex instanceof RegExp
+    && !regex.test(value)
+  ) {
+    return new TypeError(`${propName} does not pass regex tests in ${componentName}`);
+  }
+
+  if (
+    func
+    && typeof func === 'function'
+  ) return func(props, propName, componentName)
+
+  return null
 }
+
+module.exports = madProps
